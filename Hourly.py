2626cbd04e41e549283d127f19bc6a43677da7c2
m0rp43us <wailchalabi1996@gmail.com>
2021-10-06 11:13:01 -0400

    hourly and daily functions

--- Hourly.py
+++ Hourly.py
@@ -1,118 +1,118 @@
 
 
-from openmeteopy.Forcast import Forcast
-
 
 class Hourly()  :
 
+    def __init__(self) :
+        self.hourly_params = []
 
-    def temperature_2m():
-        pass
+    def temperature_2m(self):
+        self.hourly_params.append("temperature_2m")
 
-    def relativehumidity_2m():
-        pass
+    def relativehumidity_2m(self):
+        self.hourly_params.append("relativehumidity_2m")
 
-    def dewpoint_2m():
-        pass
+    def dewpoint_2m(self):
+        self.hourly_params.append("dewpoint_2m")
 
-    def apparent_temperature():
-        pass
+    def apparent_temperature(self):
+        self.hourly_params.append("apparent_temperature")
 
-    def pressure_msl():
-        pass
+    def pressure_msl(self):
+        self.hourly_params.append("pressure_msl")
 
-    def cloudcover():
-        pass
+    def cloudcover(self):
+        self.hourly_params.append("cloudcover")
 
-    def cloudcover_low():
-        pass
+    def cloudcover_low(self):
+        self.hourly_params.append("cloudcover_low")
 
-    def cloudcover_mid():
-        pass
+    def cloudcover_mid(self):
+        self.hourly_params.append("cloudcover_mid")
 
-    def cloudcover_high():
-        pass
+    def cloudcover_high(self):
+        self.hourly_params.append("cloudcover_high")
 
-    def windspeed_10m():
-        pass
+    def windspeed_10m(self):
+        self.hourly_params.append("windspeed_10m")
 
-    def windspeed_80m():
-        pass
+    def windspeed_80m(self):
+        self.hourly_params.append("windspeed_80m")
 
-    def windspeed_120m():
-        pass
+    def windspeed_120m(self):
+        self.hourly_params.append("windspeed_120m")
 
-    def windspeed_180m():
-        pass
+    def windspeed_180m(self):
+        self.hourly_params.append("windspeed_180m")
 
-    def winddirection_10m():
-        pass
+    def winddirection_10m(self):
+        self.hourly_params.append("winddirection_10m")
 
-    def winddirection_80m():
-        pass
+    def winddirection_80m(self):
+        self.hourly_params.append("winddirection_80m")
 
-    def winddirection_120m():
-        pass
+    def winddirection_120m(self):
+        self.hourly_params.append("winddirection_120m")
 
-    def winddirection_180m():
-        pass
+    def winddirection_180m(self):
+        self.hourly_params.append("winddirection_180m")
 
-    def windgusts_10m():
-        pass
+    def windgusts_10m(self):
+        self.hourly_params.append("windgusts_10m")
 
-    def shortwave_radiation():
-        pass
+    def shortwave_radiation(self):
+        self.hourly_params.append("shortwave_radiation")
 
-    def direct_radiation():
-        pass
+    def direct_radiation(self):
+        self.hourly_params.append("irect_radiation")
 
-    def diffuse_radiation():
-        pass
+    def diffuse_radiation(self):
+        self.hourly_params.append("diffuse_radiation")
 
-    def vapor_pressure_deficit():
-        pass
+    def vapor_pressure_deficit(self):
+        self.hourly_params.append("apor_pressure_deficit")
 
-    def evapotranspiration():
-        pass
+    def evapotranspiration(self):
+        self.hourly_params.append("evapotranspiration")
 
-    def precipitation():
-        pass
+    def precipitation(self):
+        self.hourly_params.append("precipitation")
 
-    def weathercode():
-        pass
+    def weathercode(self):
+        self.hourly_params.append("weathercode")
 
-    def snow_height():
-        pass
+    def snow_height(self):
+        self.hourly_params.append("snow_height")
 
-    def freezinglevel_height():
-        pass
+    def freezinglevel_height(self):
+        self.hourly_params.append("freezinglevel_height")
 
-    def soil_temperature_0cm():
-        pass
+    def soil_temperature_0cm(self):
+        self.hourly_params.append("soil_temperature_0cm")
 
-    def soil_temperature_6cm():
-        pass
+    def soil_temperature_6cm(self):
+        self.hourly_params.append("soil_temperature_6cm")
 
-    def soil_temperature_18cm():
-        pass
+    def soil_temperature_18cm(self):
+        self.hourly_params.append("soil_temperature_18cm")
 
-    def soil_temperature_54cm():
-        pass
+    def soil_temperature_54cm(self):
+        self.hourly_params.append("soil_temperature_54cm")
 
-    def soil_moisture_0_1cm():
-        pass
+    def soil_moisture_0_1cm(self):
+        self.hourly_params.append("soil_moisture_0_1cm")
 
-    def soil_moisture_1_3cm():
-        pass
+    def soil_moisture_1_3cm(self):
+        self.hourly_params.append("soil_moisture_1_3cm")
 
-    def soil_moisture_3_9cm():
-        pass
+    def soil_moisture_3_9cm(self):
+        self.hourly_params.append("soil_moisture_3_9cm")
 
-    def soil_moisture_9_27cm():
-        pass
+    def soil_moisture_9_27cm(self):
+        self.hourly_params.append("soil_moisture_9_27cm")
 
-    def soil_moisture_27_81cm():
-        pass
+    def soil_moisture_27_81cm(self):
+        self.hourly_params.append("soil_moisture_27_81cm")
 
 
 
--- OWmanager.py
+++ OWmanager.py
@@ -1,8 +1,22 @@
+import requests
 
 
-from openmeteopy.Forcast import Forcast
+class OWmanager():
 
+    url = "https://api.open-meteo.com/v1/forecast?"
 
-class OWmanager(Forcast):
+    payload = {
+            "latitude": 52.52,
+            "longitude": 13.41,
+            "hourly": "temperature_2m",
+            "daily": 'weathercode',
+            "timezone": "America/Denver",
+            }
+    
+
+    def get_data(url,payload):
+        print(requests.get(url, params = payload).content)
     pass
+
+OWmanager.get_data(OWmanager.url,OWmanager.payload)
 
