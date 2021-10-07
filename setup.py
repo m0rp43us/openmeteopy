4021805e75efdf6efd350bb733d72e4c17a389b5
m0rp43us <wailchalabi1996@gmail.com>
2021-10-07 11:31:32 -0400

    added license

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name = "openmeteopy",
+    version = "0.0.1",
+    author = "Wail Chalabi",
+    author_email = "wail.agroconcept@gmail.com",
+    description = "Download Meteorological Data from OPEN-METEO API (https://open-meteo.com/en/)",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url="https://github.com/m0rp43us/openmeteopy",
+    packages = setuptools.find_packages(),
+    license="GNU General Public License v3 or later (GPLv3+)",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires = '>=3.6',
+    install_requires=required,
+)
