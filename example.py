6b32be163af56f2039bc85405f0890658e317c93
Marco Ferrero <marcoadriano.ferrero@mail.polimi.it>
2024-02-16 12:41:06 +0100

    fixing ecmwf tests

--- example.py
+++ example.py
@@ -2,7 +2,6 @@ from openmeteo_py import OWmanager
 from openmeteo_py.Hourly.HourlyEcmwf import HourlyEcmwf
 from openmeteo_py.Options.EcmwfOptions import EcmwfOptions
 from openmeteo_py.Utils.constants import *
-import requests
 
 # Latitude, Longitude 
 latitude =  -6.31
@@ -25,15 +24,4 @@ mgr = OWmanager(options, OWmanager.ecmwf, hourly.precipitation())
 # Fetch data
 meteo = mgr.get_data()
 
-# Call OM api and check that fetching data is ok 
-API_URL = 'https://api.open-meteo.com/v1/ecmwf?latitude=-6.31&longitude=33.89&elevation=nan&timeformat=iso8601&past_days=0&temperature_unit=celsius&current_weather=False&windspeed_unit=kmh&precipitation_unit=mm&start_date=2024-01-30&end_date=2024-02-01&cell_selection=land&hourly=precipitation'
-res = requests.get(API_URL)
-
-if meteo['hourly'] == res.json()['hourly']:
-    print('SAME RESPONSE')
-else:
-    print('NOOOOO')
-    
 print(meteo)
-print('---------------------------------------------------')
-# print(res.json()['hourly'])
--- tests/test_ecmwf.py
+++ tests/test_ecmwf.py
@@ -9,20 +9,19 @@ latitude =  -6.31
 longitude = 33.89
 
 def test_row_json_forecasts():
-    start_date = '2024-01-30'
-    end_date = '2024-02-01'
     # Set a class to specify hourly forecasts for the ECMWF system
     hourly = HourlyEcmwf()
     # Set options to get specific forecasts
     options = EcmwfOptions(latitude, 
-                           longitude 
+                           longitude,
                            forecast_days=3) 
         
     # Call OM api and check that fetching data is ok 
-    API_URL = 'https://api.open-meteo.com/v1/ecmwf?latitude=-6.31&longitude=33.89&hourly=precipitation&forecast_days=5'
+    API_URL = 'https://api.open-meteo.com/v1/ecmwf?latitude=-6.31&longitude=33.89&elevation=nan&hourly=temperature_2m&forecast_days=3'
     res = requests.get(API_URL)
     # Set the OM client to fetch data
-    mgr = OWmanager(options, OWmanager.ecmwf, hourly.precipitation())
+    mgr = OWmanager(options, OWmanager.ecmwf, hourly.temperature_2m())
+    print(mgr.url, mgr.payload)
     # Fetch data
     meteo = mgr.get_data()
             
@@ -34,14 +33,14 @@ def test_row_json_historical():
     # Set a class to specify hourly forecasts for the ECMWF system
     hourly = HourlyEcmwf()
     # Set options to get specific forecasts
-    options = EcmwfOptions(latitude, 
-                        longitude, 
-                        start_end=True, # Set this flag true to get specific time window data 
-                        start_date=start_date, 
-                        end_date=end_date) 
+    options = EcmwfOptions(latitude,
+                           longitude,
+                           start_end=True, # Set this flag true to get specific time window data
+                           start_date=start_date,
+                           end_date=end_date) 
     
     # Call OM api and check that fetching data is ok 
-    API_URL = 'https://api.open-meteo.com/v1/ecmwf?latitude=-6.31&longitude=33.89&elevation=nan&timeformat=iso8601&past_days=0&temperature_unit=celsius&current_weather=False&windspeed_unit=kmh&precipitation_unit=mm&start_date=2024-01-30&end_date=2024-02-01&cell_selection=land&hourly=precipitation'
+    API_URL = 'https://api.open-meteo.com/v1/ecmwf?latitude=-6.31&longitude=33.89&start_date=2024-01-30&end_date=2024-02-01&hourly=precipitation'
     res = requests.get(API_URL)
     # Set the OM client to fetch data
     mgr = OWmanager(options, OWmanager.ecmwf, hourly.precipitation())
